284a3d2f99169bb1f598d1da0da12e033b47a0fd
Itchy <itchy@laurinmurer.ch>
2019-11-29 16:55:04 +0100

    Drafted web frontend

--- walk-flow/src/App.vue
+++ walk-flow/src/App.vue
@@ -1,28 +1,140 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1 style="text-align: center">WalkFlow: Analyzing pedestrian volumes</h1>
+		<div class="container">
+			<div class="row main">
+				<label for="usageTypesSelector">Why are people walking there?</label>
+				<b-form-select id="usageTypesSelector" v-model="usage" :options="usageTypes"></b-form-select>
+			</div>
+			<div class="row main" v-for="(measure, index) in measures">
+				<h5 class="col-auto" style="white-space: nowrap;">Measure {{index+1}}</h5>
+				<div class="col">
+					<div class="row mb-1">
+						<div class="col">
+							<label>What day did you count?</label>
+						</div>
+						<div class="col-md-4">
+							<b-form-input v-model="measure.date" type="date"></b-form-input>
+						</div>
+					</div>
+					<div class="row mb-1">
+						<div class="col">
+							<label>How many people did you count?</label>
+						</div>
+						<div class="col-md-4">
+							<b-form-input v-model="measure.counted" type="number"></b-form-input>
+						</div>
+					</div>
+					<div class="row mb-1">
+						<div class="col">
+							<label>What was the temperature like when you counted?</label>
+						</div>
+						<div class="col-md-4">
+							<div class="input-group">
+								<b-form-input v-model="measure.temperature" type="number"></b-form-input>
+								<div class="input-group-append">
+									<span class="input-group-text">°C</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row mb-1">
+						<div class="col">
+							<label>How much rain did fall when you counted?</label>
+						</div>
+						<div class="col-md-4">
+							<div class="input-group">
+								<b-form-input v-model="measure.rain" type="number"></b-form-input>
+								<div class="input-group-append">
+									<span class="input-group-text">mm</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row main">
+				<button type="button" class="btn btn-primary" @click="addMeasure">Add another counting</button>
+			</div>
+			<div class="alert alert-success" role="alert" v-if="projection">
+				<h5>Projection calculated</h5>
+				<table class="table table-bordered">
+					<tr>
+						<th>Average daily traffic</th>
+						<td>42</td>
+					</tr>
+					<tr>
+						<th>Average weekdaily traffic</th>
+						<td>49</td>
+					</tr>
+					<tr>
+						<th>Expected yearly traffic</th>
+						<td>15330</td>
+					</tr>
+				</table>
+			</div>
+			<div class="row main">
+				<pre>{{getRequestData}}</pre>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		data() {
+			return {
+				usage: null,
+				usageTypes: [
+					{value: null, text: 'unknown'},
+					{value: 'work', text: 'Work'},
+					{value: 'education', text: 'Education'},
+					{value: 'shopping', text: 'Shopping'},
+					{value: 'leisure', text: 'Leisure'},
+				],
+				measures: [{}],
+				projection: null
+			}
+		},
+		computed: {
+			getRequestData: function () {
+				return {
+					usage: this.usage,
+					measures: this.measures
+				}
+			},
+			isRequestDataValid: function () {
+				for (const measure of this.measures) {
+					if (!measure.date) {
+						return false
+					}
+					if (!measure.counted) {
+						return false
+					}
+				}
+				return true
+			}
+		},
+		methods: {
+			addMeasure() {
+				this.measures.push({})
+			}
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	.row.main {
+		margin-top: 1em;
+		margin-bottom: 1em;
+	}
 </style>

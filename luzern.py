3b87ca36c8f2fdacfbee16693ea3fdbc8456ee7d
Justus Will <justuswill@gmail.com>
2019-11-29 13:27:09 +0100

    Datenaufbereitung Basel

--- luzern.py
+++ luzern.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     data = pd.read_csv("Basel_Daten.csv", delimiter=';')
     locs = sorted(list(set(data['SiteName'])))
     data_list = {loc:data[data['SiteName'] == loc] for loc in locs}
-    data_loc_list = []
-    for loc_data in [data_list[locs[0]]]:
+    data_loc_list = {}
+    for loc, loc_data in data_list.items():
         dates = [x.split('+') for x in loc_data['DateTimeFrom'].values]
         loc_data = loc_data.assign(TimeStamp=[x[0] for x in dates], Duration=[x[1] for x in dates])
         loc_data = loc_data.set_index('TimeStamp')
-        loc_data = loc_data[['Total', 'Year', 'Month', 'Weekday', 'HourFrom']]
-        data_loc_list.append(print(loc_data))
-
-    print("%s\n%s"%(locs[0], data_loc_list[locs[0]]))
+        loc_data = loc_data[['Total', 'Duration', 'Year', 'Month', 'Weekday', 'HourFrom']]
+        data_loc_list[loc] = loc_data
+    for i in range(len(locs)):
+        print("%s\n%s\n-----\n"%(locs[i], data_loc_list[locs[i]]))

da7354a62023a83d46d20aa2ae1a3b6a979ae0dd
Justus Will <justuswill@gmail.com>
2019-11-29 13:39:47 +0100

    Duration Test

--- luzern.py
+++ luzern.py
@@ -7,9 +7,17 @@ if __name__ == '__main__':
     data_loc_list = {}
     for loc, loc_data in data_list.items():
         dates = [x.split('+') for x in loc_data['DateTimeFrom'].values]
-        loc_data = loc_data.assign(TimeStamp=[x[0] for x in dates], Duration=[x[1] for x in dates])
+        loc_data = loc_data.assign(TimeStamp=[x[0] for x in dates], Duration=[int(x[1].split(':')[0]) for x in dates])
         loc_data = loc_data.set_index('TimeStamp')
         loc_data = loc_data[['Total', 'Duration', 'Year', 'Month', 'Weekday', 'HourFrom']]
         data_loc_list[loc] = loc_data
+
+    #Analyse Durations
+    #1h 22%
+    #2h 78%
+    dur = []
     for i in range(len(locs)):
-        print("%s\n%s\n-----\n"%(locs[i], data_loc_list[locs[i]]))
+        dur.extend(data_loc_list[locs[i]]['Duration'].values)
+
+    for i in set(dur):
+        print("%s :%.2f"%(i, sum(dur == i)/len(dur)))

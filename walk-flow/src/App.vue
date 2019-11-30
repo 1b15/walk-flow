6c1732158019035a1de5567b867f9247e9e58bfc
Itchy <itchy@laurinmurer.ch>
2019-11-30 12:07:09 +0100

    Added post request to frontend

--- walk-flow/src/App.vue
+++ walk-flow/src/App.vue
@@ -3,10 +3,18 @@
 		<h1 style="text-align: center">WalkFlow: Analyzing pedestrian volumes</h1>
 		<div class="container">
 			<div class="row main">
+				<label>Where did you count?</label>
+				<b-form-select v-model="countingPoint" :options="countingPoints"></b-form-select>
+			</div>
+			<div class="row main">
+				<label>When did you count?</label>
+				<b-form-select v-model="dow" :options="dows"></b-form-select>
+			</div>
+			<div class="row main" v-if="false">
 				<label for="usageTypesSelector">Why are people walking there?</label>
 				<b-form-select id="usageTypesSelector" v-model="usage" :options="usageTypes"></b-form-select>
 			</div>
-			<div class="row main" v-for="(measure, index) in measures">
+			<div class="row main" v-for="(measure, index) in measures" v-if="false">
 				<h5 class="col-auto" style="white-space: nowrap;">Measure {{index+1}}</h5>
 				<div class="col">
 					<div class="row mb-1">
@@ -53,7 +61,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="row main">
+			<div class="row main" v-if="false">
 				<button type="button" class="btn btn-primary" @click="addMeasure">Add another counting</button>
 			</div>
 			<div class="alert alert-success" role="alert" v-if="projection">
@@ -86,6 +94,63 @@
 					{value: 'shopping', text: 'Shopping'},
 					{value: 'leisure', text: 'Leisure'},
 				],
+				countingPoint: null,
+				countingPoints: [
+					{value: null, text: 'unknown'},
+					{value: '809 Allschwilerstrasse 77/86', text: 'Allschwilerstrasse 77/86'},
+					{value: '903 Äussere Baselstrasse 328', text: 'Äussere Baselstrasse 328'},
+					{value: '911 Birskopfsteg', text: 'Birskopfsteg'},
+					{value: '913 Burgfelderstrasse', text: 'Burgfelderstrasse'},
+					{value: '405 Dorenbachviadukt', text: 'Dorenbachviadukt'},
+					{value: '350 Dreirosenbrücke', text: 'Dreirosenbrücke'},
+					{value: '817 Elisabethenanlage', text: 'Elisabethenanlage'},
+					{value: '814 Elisabethenstrasse 46*', text: 'Elisabethenstrasse 46'},
+					{value: '918 Elsässerrheinweg', text: 'Elsässerrheinweg'},
+					{value: '912 Elsässerstrasse 261/260', text: 'Elsässerstrasse 261/260'},
+					{value: '660 Flughafenstrasse', text: 'Flughafenstrasse'},
+					{value: '909 General Guisan-Strasse 104', text: 'General Guisan-Strasse 104'},
+					{value: '806 Gerbergasse', text: 'Gerbergasse'},
+					{value: '908 Grenzacherstrasse (Kraftwerk)', text: 'Grenzacherstrasse (Kraftwerk)'},
+					{value: '807 Güterstrasse 180/183', text: 'Güterstrasse 180/183'},
+					{value: '904 Hammerstrasse 90', text: 'Hammerstrasse 90'},
+					{value: '813 Hardstrasse 66/77', text: 'Hardstrasse 66/77'},
+					{value: '906 Hegenheimerstrasse 44', text: 'Hegenheimerstrasse 44'},
+					{value: '403 Heuwaage-Viadukt', text: 'Heuwaage-Viadukt'},
+					{value: '914 Hiltalingerstrasse', text: 'Hiltalingerstrasse'},
+					{value: '920 J. Burckhardt-Strasse', text: 'J. Burckhardt-Strasse'},
+					{value: '803 Johanniterbrücke', text: 'Johanniterbrücke'},
+					{value: '352 Johanniterbrücke', text: 'Johanniterbrücke'},
+					{value: '802 Klybeckstrasse 113/Kirche', text: 'Klybeckstrasse 113/Kirche'},
+					{value: '905 Leimenstrasse 4', text: 'Leimenstrasse 4'},
+					{value: '915 Luzernerring-Brücke', text: 'Luzernerring-Brücke'},
+					{value: '815 Mittlere Rheinbrücke', text: 'Mittlere Rheinbrücke'},
+					{value: '811 Mülhauserstrasse 110/122', text: 'Mülhauserstrasse 110/122'},
+					{value: '810 Neubadstrasse 124/137', text: 'Neubadstrasse 124/137'},
+					{value: '901 Peter-Merian Weg', text: 'Peter-Merian Weg'},
+					{value: '805 Rebgasse 11/28', text: 'Rebgasse 11/28'},
+					{value: '804 Rosentalstrasse 29/28', text: 'Rosentalstrasse 29/28'},
+					{value: '659 Schlachthofstrasse', text: 'Schlachthofstrasse'},
+					{value: '816 Schmiedgasse 4/7 (Riehen)', text: 'Schmiedgasse 4/7 (Riehen)'},
+					{value: '917 Schwarzwaldbrücke', text: 'Schwarzwaldbrücke'},
+					{value: '919 St. Alban-Rheinweg', text: 'St. Alban-Rheinweg'},
+					{value: '910 St. Galler-Ring 101', text: 'St. Galler-Ring 101'},
+					{value: '916 Stückisteg', text: 'Stückisteg'},
+					{value: '902 Viaduktstrasse', text: 'Viaduktstrasse'},
+					{value: '907 Wasgenring 62', text: 'Wasgenring 62'},
+					{value: '354 Wettsteinbrücke', text: 'Wettsteinbrücke'},
+					{value: '812 Wettsteinbrücke', text: 'Wettsteinbrücke'},
+					{value: '808 Wolfschlucht-Promenade', text: 'Wolfschlucht-Promenade'}
+				],
+				dow: 6,
+				dows: [
+					{value: 0, text: 'Monday'},
+					{value: 1, text: 'Tuesday'},
+					{value: 2, text: 'Wednesday'},
+					{value: 3, text: 'Thursday'},
+					{value: 4, text: 'Friday'},
+					{value: 5, text: 'Saturday'},
+					{value: 6, text: 'Sunday'},
+				],
 				measures: [{}],
 				projection: null
 			}
@@ -93,30 +158,31 @@
 		computed: {
 			getRequestData: function () {
 				return {
-					usage: this.usage,
-					measures: this.measures
+					countingPoint: this.countingPoint,
+					dow: this.dow
 				}
 			},
 			getRequestDataString: function () {
 				return JSON.stringify(this.getRequestData)
 			},
 			isRequestDataValid: function () {
-				for (const measure of this.measures) {
-					if (!measure.date) {
-						return false
-					}
-					if (!measure.counted) {
-						return false
-					}
-				}
-				return true
+				return this.countingPoint!=null
+				// for (const measure of this.measures) {
+				// 	if (!measure.date) {
+				// 		return false
+				// 	}
+				// 	if (!measure.counted) {
+				// 		return false
+				// 	}
+				// }
+				// return true
 			}
 		},
 		watch: {
 			getRequestDataString: function (newRequestData) {
 				this.projection = null
 				if (this.isRequestDataValid) {
-					axios.get('http://localhost:8000/predict', this.getRequestData).then((response) => {
+					axios.post('http://localhost:8000/predict', this.getRequestData).then((response) => {
 						this.projection = response.data
 					})
 				}
